08ef5196192adbd974c39786381f1f279a537d04
Pamarthi Prabhavika Sree <77741114+prabha2612@users.noreply.github.com>
2024-02-23 15:31:32 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import axios from "axios"; //import axios
+import styles from "./styles.module.css"; // Import styles
+
+function App() {
+  const [url, setUrl] = useState("");
+  const [status, setStatus] = useState("");
+  const [pdfUrl, setPdfUrl] = useState("");
+
+  const handleConvert = async () => {
+    //handleconvert handles the processof converting webpages to pdf files with the url
+    setStatus("Converting...");
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/convertToPdf",
+        { url },
+        { responseType: "blob" }
+      );
+      const blob = new Blob([response.data], { type: "application/pdf" });
+      const pdfUrl = window.URL.createObjectURL(blob);
+      setPdfUrl(pdfUrl);
+
+      setStatus("Converted to PDF");
+    } catch (error) {
+      setStatus("Conversion failed");
+      console.error("Error:", error);
+    }
+  };
+
+  const handleDownload = () => {
+    //handledownload handles the process of downloading the file
+    const a = document.createElement("a");
+    a.href = pdfUrl;
+    a.download = "converted.pdf";
+    document.body.appendChild(a);
+    a.click();
+    URL.revokeObjectURL(pdfUrl);
+    setUrl("");
+  };
+
+  return (
+    <body>
+      <div className={styles.container}>
+        <h1 className={styles.title}>PDF Converter</h1>
+        <input
+          type="text"
+          value={url}
+          onChange={(event) => setUrl(event.target.value)}
+          placeholder="Enter URL"
+          className={styles.input}
+        />
+        <button onClick={handleConvert} className={styles.button}>
+          Convert to PDF
+        </button>
+        {status && <div className={styles.status}>{status}</div>}
+        {pdfUrl && (
+          <button
+            type="button"
+            onClick={handleDownload}
+            className={`${styles.button} ${styles.downloadbutton}`}
+          >
+            Download
+          </button>
+        )}
+      </div>
+    </body>
+  );
+}
+
+export default App;
---
+++ webpage-to-pdf/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const puppeteer = require("puppeteer");
+const cors = require("cors");
+
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+// function to convert webpage to pdf
+app.post("/convertToPdf", async (req, res) => {
+  const { url } = req.body;
+
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: "networkidle2" });
+    const pdfBuffer = await page.pdf({ format: "A4" });
+
+    await browser.close();
+
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader("Content-Disposition", "attachment; filename=converted.pdf");
+    res.send(pdfBuffer);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("An error occurred during PDF conversion.");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
